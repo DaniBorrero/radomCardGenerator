4391b3b2baa03eccf03236a82602914f1766e9b3
DaniBorrero <borrerodaniel89@gmail.com>
2021-07-17 19:14:56 +0000

    Random card terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+var number = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
+var pinta = [1, 2, 3, 4];
+function randon(array) {
+  return [Math.floor(Math.random() * array.length)];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let x = number[randon(number)];
+  let y = randon(pinta);
+  //console.log(y);
+
+  let i = document.getElementById("numero");
+  let j = document.getElementById("pinta");
+  let k = document.getElementById("atnip");
+  i.style.fontSize = "150px";
+  j.style.fontSize = "70px";
+  k.style.fontSize = "70px";
+
+  i.innerHTML = x;
+
+  if (y == 0) {
+    j.innerHTML =
+      '<img src="https://github.githubassets.com/images/icons/emoji/unicode/2666.png"></img>';
+    k.innerHTML =
+      '<img src="https://github.githubassets.com/images/icons/emoji/unicode/2666.png"></img>';
+  } else if (y == 1) {
+    j.innerHTML =
+      '<img src="https://github.githubassets.com/images/icons/emoji/unicode/2665.png"></img>';
+    k.innerHTML =
+      '<img src="https://github.githubassets.com/images/icons/emoji/unicode/2665.png"></img>';
+  } else if (y == 2) {
+    j.innerHTML =
+      '<img src="https://github.githubassets.com/images/icons/emoji/unicode/2660.png"></img>';
+    k.innerHTML =
+      '<img src="https://github.githubassets.com/images/icons/emoji/unicode/2660.png"></img>';
+  } else {
+    j.innerHTML =
+      '<img src="https://github.githubassets.com/images/icons/emoji/unicode/2663.png"></img>';
+    k.innerHTML =
+      '<img src="https://github.githubassets.com/images/icons/emoji/unicode/2663.png"></img>';
+  }
 };
